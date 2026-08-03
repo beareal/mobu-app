8c936e3a3ada94e43160787dcd2eb6937a19296e
Lana <buntasistermoon@yahoo.co.jp>
2026-08-03 10:52:38 +0900

    深夜枠②

--- logic-router.js
+++ logic-router.js
@@ -705,7 +705,7 @@ const fakeBanner = document.getElementById('fake-notification-banner');
                      appendLineMessage('mobu', taskReactionText, initialDelay);
                      initialDelay += 1000;
                      setTimeout(() => {
-                         if (Math.random() < 0.3) {
+                         if (getMoodTimeSlot() !== 'midnight' && Math.random() < 0.3) {
                              startMoodSharing();
                          } else {
                              checkAndSetupEvent();
