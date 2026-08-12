5870a7e88ebfd029e221b33fccaed35bf6c3ef56
Lana <buntasistermoon@yahoo.co.jp>
2026-08-12 14:39:27 +0900

    ④を無かったことに

--- logic-router.js
+++ logic-router.js
@@ -1202,10 +1202,8 @@ function setupReportScreen(completedTasks) {
     if (completedTasks.length === 1) {
         currentThoughtText.textContent = 'よし、完了！モブ君に報告しよっと♪';
         currentScreen.onclick = function() {
-            checkPendingCinemaAndRoute(completedTasks[0], function() {
-                localStorage.setItem('currentReportTask', completedTasks[0]);
-                showScreen('screen-line');
-            });
+            localStorage.setItem('currentReportTask', completedTasks[0]);
+            showScreen('screen-line');
         };
     } else {
         currentThoughtText.textContent = '今日は色々頑張ったけど、特に頑張った一つをモブ君に報告しよう。';
